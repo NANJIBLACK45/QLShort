dbbda12ac462bf1a64c63d016fc075d887c1f844
Tharun1828 <91731118+Tharun1828@users.noreply.github.com>
2022-09-27 06:52:01 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -29,7 +29,7 @@ Hey! My name is {firstname}. I am a Link Convertor and Shortener Bot, here to ma
 
 I have lots of handy features, such as 
 
-- [Hyperlink](https://t.me/{username})
+- [Hyperlink](https://t.me/Rapid_bots)
 - Buttons convert support
 - Include domains 
 - Exclude domains
@@ -46,7 +46,7 @@ Helpful commands:
 - /help: Sends this message; I'll tell you more about myself!
 - /batch -100xxx: To short or convert all posts of your channel
 
-If you have any bugs or questions on how to use me, have a look at my [website]({repo}), or contact to {owner}.
+If you have any bugs or questions on how to use me, contact to {https://t.me/Rapid_Bots}.
 
 Available commands:
 
@@ -73,9 +73,9 @@ ABOUT_TEXT = """
     
 `📝 Language:` [Python 3](https://www.python.org/)
 `🧰 Framework:` [Pyrogram](https://github.com/pyrogram/pyrogram)
-`👨‍💻 Developer:` [Dev](t.me/ask_admin001)
-`📢 Support:` [Talk Bot](https://t.me/t2linkspromotion_bot)
-`🌐 Source Code:` [GitHub](https://github.com/T2links)
+`👨‍💻 Developer:` [Dev](https://t.me/Rapid_Bots)
+`📢 Support:` [Talk Bot](https://t.me/Rapid_Bots)
+`🌐 Source Code:` [GitHub](https://t.me/Rapid_Bots)
 """
 
 
@@ -101,9 +101,7 @@ Ex: https://t.me/example | Example"""
 
 
 ADMINS_MESSAGE = """
-List of Admins who has access to this Bot
-
-{admin_list}
+Fun 🔥
 """
 
 
